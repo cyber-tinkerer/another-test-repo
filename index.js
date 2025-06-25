f23fda0eef4998bb78b9ec677fa308592cff8ba6
Silvano Silva <silvano101@gmail.com>
2025-06-24 19:35:50 -0700

    Test webhook after secret update

--- index.js
+++ index.js
@@ -27,3 +27,4 @@ console.log('Final webhook test');
 // Improved signature verification test Tue Jun 24 17:24:55 PDT 2025
 // Webhook test Tue Jun 24 17:29:33 PDT 2025
 // Webhook test Tue Jun 24 19:34:33 PDT 2025
+// Another webhook test Tue Jun 24 19:35:50 PDT 2025
